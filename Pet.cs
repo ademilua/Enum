86483256a0dccd1c0a6612b79f057c3805ca4c59
Tolulope <tolesy2003@outlook.com>
2017-12-21 02:56:04 +0200

    Update Pet.cs

--- Pet.cs
+++ Pet.cs
@@ -34,7 +34,51 @@ namespace EnumerationLesson
             tortoise,
 
         }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
+
+namespace LocalDbsample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SkypletEntities db = new SkypletEntities();
+            var customers = db.Customers;
+            
+            //string result = "";
+            //foreach (var customer in customers)
+            // {
+            // result += string.Format("{0} | {1} | {2}", customer.Name, customer.CustomerId, customer.Address);
+            // }
+            customerGridView.DataSource = customers.ToList(); // Using Gridview to get data from database
+            customerGridView.DataBind();// Populate out data
+        
+            //resultLabel.Text = result;
+        }
+
+        protected void customerGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            GridViewRow row = customerGridView.Rows[index];
+            var view = row.Cells[0].Text;
+            var CustomerIdValue = row.Cells[1].Text;
+            var Name = row.Cells[2].Text;
+            var Address = row.Cells[3].Text;
+            var City = row.Cells[4].Text;
+            var Note = row.Cells[7].Text;
+            var CustomerId = Guid.Parse(CustomerIdValue);
+            resultLabel.Text = string.Format("{0} | {1} | {2} | {3} | {4}", CustomerId, Name, Address, City, Note);
+
+
+        }
+    }
+}
  
     }
 }
